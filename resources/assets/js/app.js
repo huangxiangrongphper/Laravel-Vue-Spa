ef20281ae3e2c128fca840b7cfa7d7a76b57f7b0
Flourishing <huangxiangrong827@163.com>
2018-08-25 07:20:02 +0800

    实现前端汉化功能

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,7 +8,18 @@ import VueRouter from 'vue-router'
 import router from './routes'
 import App from './components/App'
 
-Vue.use(VueRouter)
+import zh_CN from './locale/zh_CN';
+import VeeValidate, { Validator } from 'vee-validate';
+
+// Localize takes the locale object as the second argument (optional) and merges it.
+Validator.localize('zh_CN',zh_CN);
+
+// Install the Plugin.
+Vue.use(VeeValidate);
+
+
+Vue.use(VueRouter);
+
 Vue.component('app',App)
 
  new Vue({
